d8a140f46cd11c22caa604621c1a9e3d0318f524
Adebozz <ademolaadeigbe3@gmail.com>
2025-05-11 23:17:09 +0100

    `Corrected route definition and removed unused code`

--- myApp.js
+++ myApp.js
@@ -1,7 +1,11 @@
 let express = require('express');
 let app = express();
 
-app.get('Hello Express');
+// Corrected route definition
+app.get('/', (req, res) => {
+  res.send('Hello Express');
+});
+
 console.log("Hello World");
 
- module.exports = app;
+module.exports = app;
