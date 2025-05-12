8ba97c4379bd65d91b40523554fda482712cbcac
Adebozz <ademolaadeigbe3@gmail.com>
2025-05-12 23:34:39 +0100

    `Added express.static middleware to serve static assets from /public`

--- myApp.js
+++ myApp.js
@@ -2,6 +2,9 @@ let express = require('express');
 let app = express();
 let path = require('path');
 
+// Mount express.static middleware to serve static assets from /public
+app.use('/public', express.static(__dirname + '/public'));
+
 // Route to send HTML file
 app.get('/', (req, res) => {
   let absolutePath = __dirname + '/views/index.html';
