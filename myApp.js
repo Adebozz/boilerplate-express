4eaafaf62de62d43426fb3cfa1cfa6517ef72f09
Adebozz <ademolaadeigbe3@gmail.com>
2025-05-12 23:05:00 +0100

    `update`

--- myApp.js
+++ myApp.js
@@ -1,11 +1,14 @@
 let express = require('express');
 let app = express();
+let path = require('path');
 
-// Corrected route definition
+// Route to send HTML file
 app.get('/', (req, res) => {
-  res.send('Hello Express');
+  let absolutePath = __dirname + '/views/index.html';
+  res.sendFile(absolutePath);
 });
 
+// console.log for debugging
 console.log("Hello World");
 
 module.exports = app;
